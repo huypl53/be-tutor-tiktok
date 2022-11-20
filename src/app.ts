8f806f849d1ada7112670476d673ca2a89a6255e
huypl53 <phamlehuy53@gmail.com>
2022-11-20 10:44:40 +0700

    secure express app and init proj structure

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,20 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import noCache from "nocache";
+
 const app = express();
+
+function initializeSecurity() {
+  app.use(noCache());
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+}
+
+initializeSecurity();
 
 export default app;
