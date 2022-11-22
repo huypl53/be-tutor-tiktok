3ead24641c2060d8f7c6c4fb55992dd396707000
huy <phamlehuy52@gmail.com>
2022-11-23 04:18:59 +0700

    mongodb

--- src/app.ts
+++ src/app.ts
@@ -41,8 +41,12 @@ initializeErrorHandler();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+
+export const listen = async () => {
+  await initializeResources();
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
 
 export default app;
